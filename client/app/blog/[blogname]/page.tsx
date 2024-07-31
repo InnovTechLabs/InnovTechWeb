3ada419c47e600f59ad35f3ad501ae9b7ccdbaf2
Promise267 <promrizal@gmail.com>
2024-07-31 11:12:02 +0545

    added-navbar-and-footer-to-blog-detail-page

--- client/app/blog/[blogname]/page.tsx
+++ client/app/blog/[blogname]/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export default function page({params} : {params : {blogname : String}}) {
   return (
-    <div>page {params.blogname}</div>
+    <>
+    <div>
+      <Navbar/>
+      <div>page {params.blogname}</div>
+      <Footer/>
+    </div>
+    </>
   )
 }
